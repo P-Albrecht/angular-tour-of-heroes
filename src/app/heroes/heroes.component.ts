d2b6240be08808204e84a3dd09cfa97e0a3a1b00
Excd <greggseamon@gmail.com>
2023-04-08 21:17:19 -0400

    Completed pt4 'Add services'
    https://angular.io/tutorial/tour-of-heroes/toh-pt4

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { HEROES } from '../mock-heroes';
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,9 +10,23 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent {
   selectedHero?: Hero;
-  heroes = HEROES;
+  heroes: Hero[] = [];
+
+  constructor(
+    private heroService: HeroService,
+    private messageService: MessageService
+  ) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+  }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
